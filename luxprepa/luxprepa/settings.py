b850e6cb4e7f21b36aa29d8ca7d860a30f61fa5c
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-06-04 23:09:53 +0100

    settings.

--- luxprepa/luxprepa/settings.py
+++ luxprepa/luxprepa/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DEBUG=(bool,False)
 )
-environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
+if os.path.exists(os.path.join(BASE_DIR, '.env')):
+    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
