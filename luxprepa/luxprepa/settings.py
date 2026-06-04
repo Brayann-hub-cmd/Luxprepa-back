86b45d615ccca0626a6dcdeb2c72a85de2cd32f3
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-06-05 00:11:28 +0100

    settings

--- luxprepa/luxprepa/settings.py
+++ luxprepa/luxprepa/settings.py
@@ -93,12 +93,26 @@ WSGI_APPLICATION = 'luxprepa.wsgi.application'
 #         'PORT': env('DB_PORT'),
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
